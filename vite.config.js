4f933441a79b89c63e64876f223a604ccf9972b7
KevinCll <malkev.malkavian@gmail.com>
2025-05-17 17:44:06 +0200

    rebuild

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => {
   const isDev = mode === 'development'
 
   return {
-    base: process.env.NODE_ENV === "production" ? "/clinic-m/" : "/",
+    base: process.env.NODE_ENV === "production" ? "/" : "/",
     plugins: [vueDevTools(), tailwindcss(), vue()],
     resolve: {
       alias: {
